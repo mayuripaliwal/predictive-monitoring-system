0a99d2a6e755ee6960045e8b70292996ec6062b4
mayuripaliwal <mayuripaliwal8@gmail.com>
2026-09-22 11:43:37 +0530

    feat: add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import CreateMonitorForm from './CreateMonitorForm';
+function App(){
+  return (
+    <>
+    <p className="text-amber-100">Predictive Monitoring System</p>
+    <CreateMonitorForm></CreateMonitorForm>
+    </>
+  )
+}
+
+export default App
